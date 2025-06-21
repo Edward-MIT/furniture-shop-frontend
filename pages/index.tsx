aa359f6f0e04fd3a17e09da237763be303f3151e
Edward_MIT <usmanoff19980404@gmail.com>
2025-06-21 11:03:33 +0900

    feat: integraion header and footer via Layouts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
+import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Box, Container, Stack } from "@mui/material";
 import { brown, green } from "@mui/material/colors";
+import { NextPage } from "next";
 
 
-export default function Home() {
+const Home:NextPage = () => {
   return (
-    <>
-      <Stack sx={{background: green[300]}}>Header</Stack>
       <Container>
         <Stack flexBasis={"column"}>
           <Box>Popular Properties</Box>
@@ -14,7 +14,7 @@ export default function Home() {
           <Box>Events</Box>
         </Stack>
       </Container>
-      <Stack sx={{background: brown[300]}}>Footer</Stack>
-    </>
   );
 }
+
+export default withLayoutMain(Home)
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,7 +1,13 @@
-const Property = () => {
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
   return (
-    <div>Property list</div>
+    <>
+    <Container>Property list</Container>
+    </>
   )
 }
 
-export default Property;
+export default withLayoutBasic(PropertyList);
